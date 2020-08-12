d17b64264b57201e21481ac4fdfbbb18351e9b9b
vmas <vladsv88@gmail.com>
2020-08-12 12:15:57 +0300

    Fix reference to CEF binaries.

--- build-osx-app.sh
+++ build-osx-app.sh
@@ -52,7 +52,7 @@ EOF
 BINARYNAME="AvaloniaApp"
 CONFIGURATION="Debug"
 NETTARGET="netcoreapp3.1"
-CEFZIP="cef_binary_84.3.8%2Bgc8a556f%2Bchromium-84.0.4147.105_macosx64_minimal.tar.bz2"
+CEFZIP="cef_binary_84.4.0%2Bg304e015%2Bchromium-84.0.4147.105_macosx64_minimal.tar.bz2"
 TARGET="bin"
 
 
