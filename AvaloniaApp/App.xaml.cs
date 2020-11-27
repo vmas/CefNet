fa5329924ee08db2022d1e12c9ba4bc19f05c0bc
vmas <vladsv88@gmail.com>
2020-11-27 18:30:48 +0300

    Move initialization code into the Main (fix issue #36)

--- AvaloniaApp/App.xaml.cs
+++ AvaloniaApp/App.xaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Avalonia.Threading;
 using CefNet;
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -13,9 +14,8 @@ namespace AvaloniaApp
 {
 	public class App : Application
 	{
-		private CefAppImpl app;
-		private Timer messagePump;
-		private int messagePumpDelay = 10;
+		public static event EventHandler FrameworkInitialized;
+		public static event EventHandler FrameworkShutdown;
 
 		public override void Initialize()
 		{
@@ -36,64 +36,14 @@ namespace AvaloniaApp
 
 		private void Startup(object sender, ControlledApplicationLifetimeStartupEventArgs e)
 		{
-			string cefPath;
-			bool externalMessagePump = e.Args.Contains("--external-message-pump");
-
-			if (PlatformInfo.IsMacOS)
-			{
-				externalMessagePump = true;
-				cefPath = Path.Combine(GetProjectPath(), "Contents", "Frameworks", "Chromium Embedded Framework.framework");
-			}
-			else
-			{
-				cefPath = Path.Combine(Path.GetDirectoryName(GetProjectPath()), "cef");
-			}	
-
-			var settings = new CefSettings();
-			settings.MultiThreadedMessageLoop = !externalMessagePump;
-			settings.ExternalMessagePump = externalMessagePump;
-			settings.NoSandbox = true;
-			settings.WindowlessRenderingEnabled = true;
-			settings.LocalesDirPath = Path.Combine(cefPath, "Resources", "locales");
-			settings.ResourcesDirPath = Path.Combine(cefPath, "Resources");
-			settings.LogSeverity = CefLogSeverity.Warning;
-			settings.IgnoreCertificateErrors = true;
-			settings.UncaughtExceptionStackSize = 8;
-
-			app = new CefAppImpl();
-			app.ScheduleMessagePumpWorkCallback = OnScheduleMessagePumpWork;
-			app.Initialize(PlatformInfo.IsMacOS ? cefPath : Path.Combine(cefPath, "Release"), settings);
-
-			if (externalMessagePump)
-			{
-				messagePump = new Timer(_ => Dispatcher.UIThread.Post(CefApi.DoMessageLoopWork), null, messagePumpDelay, messagePumpDelay);
-			}
+			FrameworkInitialized?.Invoke(this, EventArgs.Empty);
 		}
 
 		private void Exit(object sender, ControlledApplicationLifetimeExitEventArgs e)
 		{
-			messagePump?.Dispose();
-			app?.Shutdown();
+			FrameworkShutdown?.Invoke(this, EventArgs.Empty);
 		}
 
-		private async void OnScheduleMessagePumpWork(long delayMs)
-		{
-			await Task.Delay((int)delayMs);
-			Dispatcher.UIThread.Post(CefApi.DoMessageLoopWork);
-		}
 
-		private static string GetProjectPath()
-		{
-			string projectPath = Path.GetDirectoryName(typeof(App).Assembly.Location);
-			string projectName = PlatformInfo.IsMacOS ? "AvaloniaApp.app" : "AvaloniaApp";
-			string rootPath = Path.GetPathRoot(projectPath);
-			while (Path.GetFileName(projectPath) != projectName)
-			{
-				if (projectPath == rootPath)
-					throw new DirectoryNotFoundException("Could not find the project directory.");
-				projectPath = Path.GetDirectoryName(projectPath);
-			}
-			return projectPath;
-		}
 	}
 }
